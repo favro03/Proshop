84ea47ecd2810e17b12b2c89f0eb9f2a8b4ffc35
tfavro <wetr9902@gmail.com>
2023-02-05 12:24:53 -0600

    added routing and link

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,18 +2,25 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Container } from 'react-bootstrap'
 import HomeScreen from "./screens/HomeScreen";
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import ProductScreen from "./screens/ProductScreen";
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
     <Header />
+   
     <main className="py-3">
       <Container>
-        <HomeScreen />
+        <Routes>
+          <Route path='/' element={<HomeScreen />} exact />
+          <Route path='/product/:id' element={<ProductScreen />} />
+
+        </Routes>
       </Container>
     </main>
+  
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
