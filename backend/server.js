8a927180580a7b91a50b635dc3e6525e4e8110f5
tfavro <wetr9902@gmail.com>
2023-02-06 15:31:31 -0600

    set up the data base and connection

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,15 @@ import express from 'express'
 //must have js in these files for import to work.
 import products from './data/products.js'
 import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import colors from 'colors'
 
 dotenv.config()
+connectDB()
 const app = express()
 const PORT = process.env.PORT || 5001
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
 
 
 app.get('/', (req, res) => {
