df8cef1cbec3f44d0db7257e52046245d6a11408
tfavro <wetr9902@gmail.com>
2023-02-07 11:51:23 -0600

    added error handeling

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,11 @@ import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import colors from 'colors'
 import productRoutes from './routes/productRoutes.js'
-
+import {notFound, errorHandler} from './middleware/errorMiddleware.js'
 dotenv.config()
 connectDB()
 const app = express()
+
 const PORT = process.env.PORT || 5001
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
@@ -18,3 +19,6 @@ app.get('/', (req, res) => {
 })
 
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
