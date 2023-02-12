3efc08ab43ea455ebce0f2ccfd315a626dab4a8f
tfavro <wetr9902@gmail.com>
2023-02-12 13:08:48 -0600

    checkout screen added

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -9,6 +9,7 @@ import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import ProfileScreen from "./screens/ProfileScreen";
 import ShippingScreen from "./screens/ShippingScreen"
+import PaymentScreen from "./screens/PaymentScreen";
 
 const App = () => {
   return (
@@ -19,6 +20,7 @@ const App = () => {
       <Container>
         <Routes>
           <Route path='/shipping' element={<ShippingScreen />} exact />
+          <Route path='/payment' element={<PaymentScreen />} exact />
           <Route path='/login' element={<LoginScreen />} exact />
           <Route path='/register' element={<RegisterScreen />} exact />
           <Route path='/profile' element={<ProfileScreen />} exact />
--- front-end/src/reducers/cartReducers.js
+++ front-end/src/reducers/cartReducers.js
@@ -1,4 +1,9 @@
-import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_SHIPPING_ADDRESS} from '../constants/cartConstants'
+import { 
+  CART_ADD_ITEM, 
+  CART_REMOVE_ITEM, 
+  CART_SAVE_SHIPPING_ADDRESS,
+  CART_SAVE_PAYMENT_METHOD,
+} from '../constants/cartConstants'
 
 export const cartReducer = (
     state = { cartItems: [], shippingAddress: {} },
@@ -33,6 +38,12 @@ export const cartReducer = (
             ...state,
             shippingAddress: action.payload,
         }
+        case CART_SAVE_PAYMENT_METHOD:
+          return {
+              ...state,
+              paymeetMethod: action.payload,
+          }
+          
         
         default:
             return state
