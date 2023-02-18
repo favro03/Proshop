de2e15cd567756a5bb01ae479a88b61e67917552
tfavro <wetr9902@gmail.com>
2023-02-18 14:16:27 -0600

    added a carousel of top items

--- front-end/src/screens/HomeScreen.js
+++ front-end/src/screens/HomeScreen.js
@@ -1,12 +1,13 @@
 import React, {useEffect} from 'react'
 import { useDispatch, useSelector} from 'react-redux'
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
 import { listProducts } from '../actions/productActions'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
 import Paginate from '../components/Paginate'
+import ProductCarousel from '../components/ProductCarousel'
 
 
 const HomeScreen = () => {
@@ -25,14 +26,14 @@ const HomeScreen = () => {
   
   return (
     <>
-      {/* <Meta />
+      {/* <Meta /> */}
       {!keyword ? (
         <ProductCarousel />
       ) : (
         <Link to='/' className='btn btn-light'>
           Go Back
         </Link>
-      )} */}
+      )}
       <h1>Latest Products</h1>
       {loading ? (
         <Loader />
